e4dd4f39d80bce9fa02c5602ae42b7a11b50ba97
Simon Pitt <si_pitt@yahoo.com>
2023-05-21 23:04:11 +0100

    Remove file reading. Tidy up.
    Removed file reading - put data into a table.
    Tidied up - removed unnecessary functions and comments.

--- lua/txtin/diacritics.lua
+++ lua/txtin/diacritics.lua
@@ -1,3 +1,122 @@
+local treasure = {
+  "α # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ά # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ὰ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾶ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἀ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἄ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἂ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἆ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἁ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἅ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἃ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ἇ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾴ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾲ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾷ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾀ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾄ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾂ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾆ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾁ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾅ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾃ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ᾇ # α ά ὰ ᾶ ἀ ἄ ἂ ἆ ἁ ἅ ἃ ἇ ᾳ ᾴ ᾲ ᾷ ᾀ ᾄ ᾂ ᾆ ᾁ ᾅ ᾃ ᾇ alpha",
+  "ε # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "έ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "ὲ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "ἐ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "ἔ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "ἒ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "ἑ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "ἕ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "ἓ # ε έ ὲ ἐ ἔ ἒ ἑ ἕ ἓ epsilon",
+  "η # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ή # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ὴ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ῆ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἠ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἤ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἢ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἦ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἡ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἥ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἣ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ἧ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ῄ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ῂ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ῇ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾐ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾔ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾒ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾖ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾑ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾕ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾓ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ᾗ # η ή ὴ ῆ ἠ ἤ ἢ ἦ ἡ ἥ ἣ ἧ ῃ ῄ ῂ ῇ ᾐ ᾔ ᾒ ᾖ ᾑ ᾕ ᾓ ᾗ eta",
+  "ι # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ί # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ὶ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ῖ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἰ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἴ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἲ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἶ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἱ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἵ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἳ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ἷ # ι ί ὶ ῖ ἰ ἴ ἲ ἶ ἱ ἵ ἳ ἷ iota",
+  "ο # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "οἱ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "οἳ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ό # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ὸ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ὀ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ὄ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ὂ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ὁ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ὅ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "ὃ # ο ό ὸ ὀ ὄ ὂ ὁ ὅ ὃ omicron",
+  "υ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ύ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὺ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ῦ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὐ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὔ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὒ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὖ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὑ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὕ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὓ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ὗ # υ ύ ὺ ῦ ὐ ὔ ὒ ὖ ὑ ὕ ὓ ὗ upsilon",
+  "ω # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ώ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὼ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ῶ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὠ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὤ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὢ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὦ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὡ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὥ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὣ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ὧ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ῴ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ῲ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ῷ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾠ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾤ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾢ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾦ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾡ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾥ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾣ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ᾧ # ω ώ ὼ ῶ ὠ ὤ ὢ ὦ ὡ ὥ ὣ ὧ ῳ ῴ ῲ ῷ ᾠ ᾤ ᾢ ᾦ ᾡ ᾥ ᾣ ᾧ omega",
+  "ρ # ρ ῤ ῥ rho",
+  "ῤ # ρ ῤ ῥ rho",
+  "ῥ # ρ ῤ ῥ rho",
+}
+
 local M = {}
 
 M.popup_diacritic_words = function()
@@ -5,15 +124,15 @@ M.popup_diacritic_words = function()
   local cur_wrd = vim.fn.getreg('"')
   -- P(cur_wrd)
   local filtered = {}
-  local tokens = M.get_diacritic_words()
-  for _, entry in ipairs(tokens) do
+  -- local tokens = M.get_diacritic_words()
+  -- for _, entry in ipairs(tokens) do
+  for _, entry in ipairs(treasure) do
     if entry:find(cur_wrd) then
       local head = vim.fn.split(entry, " #")[1]
       table.insert(filtered, head)
     end
   end
   local mappings = {
-    -- ['['] = 'update_view(-1)',
     ["<cr>"] = 'yiw:q<CR>vp',
   }
   M.floating_win(filtered, mappings)
@@ -43,12 +162,9 @@ M.floating_win = function(tokens, keymap)
   -- get cursor position
   local pos = vim.api.nvim_win_get_cursor(0)
   local line, col = pos[1], pos[2]
-  -- P(line)
-  -- P(col)
   -- calculate our floating window size
   local win_height = #tokens
   local win_width = longest + 2
-
   -- set some options
   local opts = {
     style = "minimal",
@@ -62,33 +178,8 @@ M.floating_win = function(tokens, keymap)
   }
 
   -- and finally create it with buffer attached
-  -- local win
-  -- win = vim.api.nvim_open_win(new_buf, true, opts)
   vim.api.nvim_open_win(new_buf, true, opts)
   vim.api.nvim_buf_set_lines(new_buf, 0, -1, false, tokens)
-  -- customize highlighting
-  -- vim.cmd [[highlight MyNormalFloat gui=italic guifg=#a0a8b0 guibg=#000000]]
-  -- vim.api.nvim_win_set_option(win, "winhl", "NormalFloat:MyNormalFloat")
-  -- vim.api.nvim_win_set_option(win, "winhl", "NormalFloat:NormalFloat")
-end
-
-M.get_diacritic_words = function()
-  local tokens = M.read_array(
-    "/home/simon/.config/nvim/lua/common_greek.txt"
-  )
-  return tokens
-end
-
-M.read_array = function(file)
-  local arr = {}
-  local handle = assert(io.open(file, "r"))
-  local value = handle:read("*line")
-  while value do
-    table.insert(arr, value)
-    value = handle:read("*line")
-  end
-  handle:close()
-  return arr
 end
 
 return M
