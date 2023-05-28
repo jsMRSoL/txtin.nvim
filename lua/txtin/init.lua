9ccd66aa6d773f7952d1e1dc0ccd7ebef63253f8
Simon Pitt <si_pitt@yahoo.com>
2023-05-28 20:00:18 +0100

    Set user commands in lua.

--- lua/txtin/init.lua
+++ lua/txtin/init.lua
@@ -1,12 +1,51 @@
 local M = {}
 
 M.setup = function()
-  vim.cmd("command! -range -nargs=0 JoinLines :lua require('txtin.trans').join_lines(<line1>, <line2>)")
-  vim.cmd("command! -nargs=0 ProcessFile lua require('txtin.clean').process_file()<CR>")
-  vim.cmd("command! -nargs=0 ShortenDictEntries lua require('txtin.trans').shorten_dictionary_entries()<CR>")
-  vim.cmd("command! -nargs=0 RemoveCommon lua require('txtin.clean').remove_common_words()<CR>")
-  vim.cmd [[command! -range NumberLines lua require('txtin.trans').number_lines(<line1>, <line2>)]]
-  vim.cmd [[command! -range AlignOnChar lua require('txtin.trans').align_on_char(<line1>, <line2>)]]
+  local cmd = vim.api.nvim_create_user_command
+
+  cmd('JoinLines', function(opts)
+    require('txtin.trans').join_lines(opts.line1, opts.line2)
+  end, { range = true })
+
+  cmd('ProcessFile', function()
+    require('txtin.clean').process_file()
+  end, {})
+
+  cmd('ShortenDictEntries', function()
+    require('txtin.trans').shorten_dictionary_entries()
+  end, {})
+
+  cmd('RemoveCommon', function()
+    require('txtin.clean').remove_common_words()
+  end, {})
+
+  cmd('NumberLines', function(opts)
+    require('txtin.trans').number_lines(opts.line1, opts.line2)
+  end, { range = true })
+
+  cmd('AlignOnChar', function(opts)
+    require('txtin.trans').align_on_char(opts.line1, opts.line2)
+  end, { range = true })
+
+  cmd('FlushLinesRegexp', function()
+    require('txtin.trans').flush_lines()
+  end, {})
+
+  cmd('KeepLines', function()
+    require('txtin.trans').keep_lines()
+  end, {})
+
+  cmd('FlushEmptyLines', function()
+    require('txtin.trans').flush_empty_lines()
+  end, {})
+
+  cmd('PopupDiacritics', function()
+    require('txtin.diacritics').popup_diacritic_words()
+  end, {})
+
+  cmd('SplitPara', function()
+    require('txtin.trans').para_to_lines()
+  end, {})
 end
 
 return M
